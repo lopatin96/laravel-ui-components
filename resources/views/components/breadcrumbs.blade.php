826c8b835d77a40b40c14ae1f10ae6e902506dbc
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2024-01-04 12:10:43 +0100

    breadcrumbs

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,26 +1,19 @@
-<div class="flex justify-between items-center w-full p-4">
-    <nav class="py-3 rounded-md w-full">
-        <ol class="flex space-x-2">
-            @foreach($breadcrumbs as $breadcrumb)
-                @isset($breadcrumb['route'])
-                    <li>
-                        <a href="/{{ $breadcrumb['route'] }}" class="text-link flex space-x-1 items-center">
-                            @if($loop->first)
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                                </svg>
-                            @endif
-                            <span>{!! $breadcrumb['title'] !!}</span>
-                        </a>
-                    </li>
-                @else
-                    <li class="text-gray-500">{!! $breadcrumb['title'] !!}</li>
-                @endisset
-
-                @if(! $loop->last)
-                    <li class="text-gray-500">/</li>
-                @endif
-            @endforeach
-        </ol>
-    </nav>
-</div>
+<ol class="flex items-center whitespace-nowrap" aria-label="Breadcrumb">
+    @foreach($breadcrumbs as $breadcrumb)
+        @isset($breadcrumb['route'])
+            <li class="inline-flex items-center">
+                <a
+                    class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600"
+                    href="{{ route($breadcrumb['route']) }}"
+                >
+                    {{ $breadcrumb['title'] }}
+                </a>
+                <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+            </li>
+        @else
+            <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate" aria-current="page">
+                {{ $breadcrumb['title'] }}
+            </li>
+        @endisset
+    @endforeach
+</ol>
