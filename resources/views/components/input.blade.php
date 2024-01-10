a5b63e2af394e081e3453691d8324f2b813e6bff
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2024-01-10 13:21:46 +0100

    input and textarea

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,15 @@
+@props(['disabled' => false, 'counter' => false])
+
+<div class="relative" x-data="{ count: 0 }" x-init="count = $refs.counter.value.length">
+    <input
+        {{ $disabled ? 'disabled' : '' }}
+        {!! $attributes->merge(['class' => 'border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 rounded-lg shadow-sm']) !!}
+        x-ref="counter"
+        x-on:keyup="count = $refs.counter.value.length"
+    />
+    @if($counter)
+        <div class="absolute right-0 text-sm mt-1 text-gray-400">
+            <span x-html="count"></span> / <span x-html="$refs.counter.maxLength"></span>
+        </div>
+    @endif
+</div>
---
+++ resources/views/components/textarea.blade.php
@@ -0,0 +1,17 @@
+@props(['disabled' => false, 'counter' => false])
+
+<div class="relative" x-data="{ count: 0 }" x-init="count = $refs.counter.value.length">
+    <textarea
+        {{ $disabled ? 'disabled' : '' }}
+        {!! $attributes->merge(['class' => 'soft-scrollbar border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 rounded-lg shadow-sm']) !!}
+        autocomplete="off"
+        rows="4"
+        x-ref="counter"
+        x-on:keyup="count = $refs.counter.value.length"
+    >{{ $slot }}</textarea>
+    @if($counter)
+        <div class="absolute right-0 text-sm mt-1 text-gray-400">
+            <span x-html="count"></span> / <span x-html="$refs.counter.maxLength"></span>
+        </div>
+    @endif
+</div>
