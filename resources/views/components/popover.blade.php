6fd4c1479380c1008f7dbbfdf01319bcf73348b9
vladyslavlopatin <ilopatin3@gmail.com>
2024-10-27 21:15:48 +0100

    add maxW and placement parameters

--- resources/views/components/popover.blade.php
+++ resources/views/components/popover.blade.php
@@ -1,6 +1,6 @@
-@props(['title'])
+@props(['title', 'maxW' => 'max-w-lg', 'placement' => null])
 
-<div class="hs-tooltip [--trigger:click] inline-block">
+<div class="hs-tooltip inline-block [--trigger:click] @if($placement) [--placement:{{ $placement }}] @endif">
     <div class="hs-tooltip-toggle block text-center">
         <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">
             {{ $title }}
@@ -9,7 +9,7 @@
             </svg>
         </button>
 
-        <div class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible hidden opacity-0 transition-opacity absolute invisible z-20 max-w-lg w-full bg-white border border-gray-100 text-start rounded-xl shadow-md" role="tooltip">
+        <div class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible hidden opacity-0 transition-opacity absolute invisible z-20 {{ $maxW }} w-full bg-white border border-gray-100 text-start rounded-xl shadow-md" role="tooltip">
             <div class="p-4">
                 {{ $slot }}
             </div>
