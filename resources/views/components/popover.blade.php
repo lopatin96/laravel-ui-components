12545c67f175d5ce1d3ff9e27b5d0b9bebe732b2
vladyslavlopatin <ilopatin3@gmail.com>
2024-10-27 15:34:10 +0100

    rotate-180

--- resources/views/components/popover.blade.php
+++ resources/views/components/popover.blade.php
@@ -4,7 +4,7 @@
     <div class="hs-tooltip-toggle block text-center">
         <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">
             {{ $title }}
-            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg class="shrink-0 size-4 rotate-180" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="m18 15-6-6-6 6"></path>
             </svg>
         </button>
