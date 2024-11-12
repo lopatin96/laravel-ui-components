6e0e1caa4aa4031fac630a592ac8f4e0f33170ca
Vladyslav Lopatin <lopatin@wskz.pl>
2024-11-12 11:36:45 +0100

    href

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
-@props(['color' => 'blue', 'title', 'button'])
+@props(['color' => 'blue', 'title', 'button', 'href'])
 
 <div class="relative flex items-center justify-between space-x-4 sm:space-x-10 border-y border-x-0 sm:border-x border-{{ $color }}-600 bg-gradient-to-br from-{{ $color }}-500 via-{{ $color }}-400 to-{{ $color }}-500 py-6 px-6 sm:px-10 sm:rounded-2xl w-full text-white">
     <div class="flex flex-col gap-6">
